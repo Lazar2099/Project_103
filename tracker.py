d79104a2012c2f2f82d9698cdedabfdf49f4a7ed
Lazar2099 <74484199+Lazar2099@users.noreply.github.com>
2023-08-06 21:42:58 +0530

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,44 @@
+import time
+import os
+import shutil
+import random
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/sisir/Downloads"
+to_dir = "C:/Users/sisir/Downloads/FILES"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+class FileMovementHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        name,ext=os.path.splitext(event.src_path)
+        time.sleep(1)
+        for key,value in dir_tree.items():
+            time.sleep(1)
+            if ext in value :
+                file_name = os.path.basename(event.src_path)
+                print("downloaded" + file_name)
+                path1 = from_dir+"/"+file_name
+                path2 = to_dir+"/"+ key
+                path3 = to_dir+"/"+key+"/"+file_name
+                if os.path.exists(path2):
+                    print("moving") 
+                    shutil.move(path1,path3)
+                    time.sleep(1)
+                else:
+                    os.makedirs(path2)
+                    print("moving") 
+                    shutil.move(path1,path3)
+                    time.sleep(1)
+
+event_handler = FileMovementHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
